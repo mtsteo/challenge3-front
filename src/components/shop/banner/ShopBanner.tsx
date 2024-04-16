4a4751da30df3284922ab77fe9b12e5b6083a484
mtsteo <mateusteotonho@gmail.com>
2024-04-16 16:12:22 -0300

    fix: linked added on home option

--- src/components/shop/banner/ShopBanner.tsx
+++ src/components/shop/banner/ShopBanner.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ShopBanner from '../../../assets/images/ShopBanner.jpg'
+import { Link } from 'react-router-dom'
 
 
 export default function () {
@@ -9,7 +10,7 @@ export default function () {
             <div className='absolute top-[50%] right-[50%]'>
                 <div className='text-5xl font-medium'>Shop</div>
                 <div className='flex gap-2 mt-4 justify-center items-center'>
-                    <h1 className='text-lg font-medium'>Home</h1>
+                    <Link to="/home" className='text-lg font-medium'>Home</Link>
                     <h1 className='font-medium text-2xl'>&gt;</h1>
                     <h1 className='font-normal'>Shop</h1>
                 </div>
