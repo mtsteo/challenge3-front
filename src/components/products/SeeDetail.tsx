bfe6b296c09bbb91d1a5a65c8fb4f7605c5e0674
mtsteo <mateusteotonho@gmail.com>
2024-04-16 09:20:17 -0300

    feat: smooth effect applied on see detail modal

--- src/components/products/SeeDetail.tsx
+++ src/components/products/SeeDetail.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function SeeDetail() {
   return (
-    <div className=" bg-black bg-opacity-50 absolute z-10 h-[100%] w-[100%] flex justify-center items-center">
-      <button className="bg-white w-48 h-12 font-semibold text-[#B88E2F]">
+    <div className="transition duration-500 hover:bg-black hover:bg-opacity-50 absolute z-10 h-[100%] w-[100%] flex justify-center items-center">
+      <button className=" bg-white w-48 h-12 font-semibold text-[#B88E2F]">
         See Details
       </button>
     </div>
