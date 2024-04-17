c8eb8db93e6545cddbe56644201afb13f72430e9
mtsteo <mateusteotonho@gmail.com>
2024-04-17 11:46:39 -0300

    feat: underline menu option removed

--- src/components/template/header/MenuButton.tsx
+++ src/components/template/header/MenuButton.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function MenuButton(props: any) {
     return (
-        <Link to={props.path} className="transition hover:scale-105 duration-75 hover:font-semibold hover:underline text-md font-medium self-center w-fit">
+        <Link to={props.path} className="transition hover:scale-105 duration-75 hover:font-semibold text-md font-medium self-center w-fit">
             {props.name}
         </Link>
     )
