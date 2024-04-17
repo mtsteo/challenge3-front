18750831b8406cd39ebc64868932ebd8ad35522c
mtsteo <mateusteotonho@gmail.com>
2024-04-17 16:44:24 -0300

    feat: product detail description part started

--- src/pages/product-detailed/details/ProductDetails.tsx
+++ src/pages/product-detailed/details/ProductDetails.tsx
@@ -3,23 +3,46 @@ import ProductMini from '../../../components/products/ProductMini'
 
 export default function () {
 
-  const images = []
 
-  
   return (
     <section className='mt-12'>
       <div className='grid grid-cols-2'>
         <div className='flex justify-center items-center ml-10'>
-          <div className='flex flex-row'>
+          <div className='flex flex-row ml-10'>
             <div className='flex flex-col gap-4'>
-              {Array.from({length : 4}, ()=>(<ProductMini img="https://i.postimg.cc/KYM5wx34/sofas.png"/>))}
+              {Array.from({ length: 4 }, () => (<ProductMini img="https://i.postimg.cc/KYM5wx34/sofas.png" />))}
             </div>
             <div className='flex justify-center items-center'>
               <img className='rounded-2xl' src="https://i.postimg.cc/KYM5wx34/sofas.png" alt="" />
             </div>
           </div>
         </div>
-        <div className='flex justify-center items-center  bg-red-600' >details</div>
+        <div className='flex flex-col bg-red-600 ' >
+          <h1>Product Name</h1>
+          <h1>Product price</h1>
+          <div className='flex flex-row gap-4'>
+            <div>stars</div>
+            <h1>Costumer review</h1>
+          </div>
+          <h1>Product Description</h1>
+          <h1>Size</h1>
+          <div className=' flex flex-row gap-4'>
+            <div>L</div>
+            <div>XL</div>
+            <div>XS</div>
+          </div>
+          <h1>color</h1>
+          <div className=' flex flex-row gap-4'>
+            <div>color1</div>
+            <div>color2</div>
+            <div>color3</div>
+          </div>
+          <div className='flex flex-row gap-3'>
+            <div>amount</div>
+            <div>add to cart</div>
+            <div>compare</div>
+          </div>
+        </div>
       </div>
     </section>
   )
