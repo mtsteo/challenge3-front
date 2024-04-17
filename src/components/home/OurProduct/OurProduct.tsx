47a81a6839e83b8fb8ab5db93e4716af3475a350
mtsteo <mateusteotonho@gmail.com>
2024-04-16 23:02:19 -0300

    fix: set lilmit of 8 products

--- src/components/home/OurProduct/OurProduct.tsx
+++ src/components/home/OurProduct/OurProduct.tsx
@@ -10,12 +10,15 @@ export default function OurProduct() {
       </div>
       <div className="flex justify-center items-center">
         <div className="grid grid-cols-4 w-4/6">
-          {data.map((prod, idx) => {
-          return <ProductCard key={prod.id} data={prod}/>})}
+          {data.slice(0, 8).map((prod, idx) => {
+            return <ProductCard key={prod.id} data={prod} />;
+          })}
         </div>
       </div>
       <div className="flex justify-center items-center mt-10">
-        <button className="w-60 h-12 border-2 border-[#B88E2F]  text-[#B88E2F] " >Show more</button>
+        <button className="w-60 h-12 border-2 border-[#B88E2F]  text-[#B88E2F] ">
+          Show more
+        </button>
       </div>
     </section>
   );
