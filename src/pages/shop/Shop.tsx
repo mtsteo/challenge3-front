f990ea29ba35bb0ad0a63f3cf00cc1682745851a
mtsteo <mateusteotonho@gmail.com>
2024-04-20 03:07:11 -0300

    feat: Outlet applied

--- src/pages/shop/Shop.tsx
+++ src/pages/shop/Shop.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import ShopBanner from './banner/ShopBanner'
-import Filter from './filter/Filter'
-import ProductList from './product-list/ProductList'
-import GoToTop from '../../utils/GotoTop'
+import React from "react";
+import ShopBanner from "./banner/ShopBanner";
+import Filter from "./filter/Filter";
+import ProductList from "./product-list/ProductList";
+import GoToTop from "../../utils/GotoTop";
+import { Outlet } from "react-router-dom";
 
 export default function Shop() {
   return (
     <>
       <ShopBanner />
       <Filter />
-      <ProductList />
+      <Outlet />
       <GoToTop />
     </>
-
-  )
+  );
 }
