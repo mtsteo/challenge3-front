9471efb644020a5f0ef855491d158179c00cfb44
mtsteo <mateusteotonho@gmail.com>
2024-04-19 22:49:54 -0300

    feat: GetByAmount method added

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,6 @@ import axios, { AxiosResponse } from "axios";
 
 const instance = axios.create({
   baseURL: "http://localhost:3001/",
-  
 });
 
 const responseBody = (response: AxiosResponse) => response.data;
@@ -11,10 +10,13 @@ const requests = {
   get: async (url: string) => await instance.get(url).then(responseBody),
 };
 
-export const Api = {
+export const ApiFetcher = {
   getAllproducts: async () => await requests.get("/products"),
-  getAllCategories: async () =>{ 
-    console.log("first")
-    return await requests.get("/categories")},
-  getBtcategories : async (name:string) =>await requests.get(`/categories/${name}`)
+  getByAmount: async (amount: number) =>
+    await requests.get(`/products/?amount${amount}`),
+  getAllCategories: async () => {
+    return await requests.get("/categories");
+  },
+  getByCategories: async (name: string) =>
+    await requests.get(`/categories/${name}`),
 };
