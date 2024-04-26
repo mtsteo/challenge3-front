3b401dc2a3967f946644f6470f45f91e68d00425
mtsteo <mateusteotonho@gmail.com>
2024-04-25 22:36:39 -0300

    feat: hoover effect on mini images

--- src/components/products/ProductMini.tsx
+++ src/components/products/ProductMini.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ProductMini(props: any) {
     return (
-        <div onClick={()=>props.setImage(props.img)} className='w-20 hover:cursor-pointer'>
+        <div onClick={()=>props.setImage(props.img)} className='w-20 hover:cursor-pointer hover:scale-95 transition duration-500 '>
             <img className='rounded-2xl w-[80px] h-[80px]' src={props.img} alt="" />
         </div>
     )
