d5842edaef14f389a19ca17cbeb73fb88d4fd7db
mtsteo <mateusteotonho@gmail.com>
2024-04-25 22:33:54 -0300

    feat: image select on produtc detail page ok

--- src/components/products/ImagesSection.tsx
+++ src/components/products/ImagesSection.tsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductMini from "../../components/products/ProductMini";
 
 export default function ImagesSection({ data }: any) {
+  const [product] = data
+  const [image, setImage] = useState(product.image_link)
+  
   return (
     <div className="flex justify-end">
       <div className="flex flex-row mr-14">
         <div className="flex flex-col gap-4 mr-6">
-          {data[0].other_images_link.map((prod: any) => (
-            <ProductMini key={Math.floor(Math.random() * 999)} img={prod} />
+          {product.other_images_link.map((prod: any) => (
+            <ProductMini key={Math.floor(Math.random() * 999)} setImage={setImage} img={prod} />
           ))}
         </div>
         <div>
-          <img className="rounded-2xl w-[500px] h-[500px]" src={data[0].image_link} alt="" />
+          <img className="rounded-2xl w-[500px] h-[500px]" src={image} alt="" />
         </div>
       </div>
     </div>
--- src/components/products/ProductMini.tsx
+++ src/components/products/ProductMini.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function ProductMini(props: any) {
     return (
-        <div className='w-20 hover:cursor-pointer'>
-            <img className='rounded-2xl' src={props.img} alt="" />
+        <div onClick={()=>props.setImage(props.img)} className='w-20 hover:cursor-pointer'>
+            <img className='rounded-2xl w-[80px] h-[80px]' src={props.img} alt="" />
         </div>
     )
 }
