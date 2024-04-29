39bf377798e39734fff5b0b96d29f5e7cdcec19c
mtsteo <mateusteotonho@gmail.com>
2024-04-28 22:56:44 -0300

    feat: api ip address added

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: "http://143.110.238.188:3001/",
 });
 
 const responseBody = (response: AxiosResponse) => response.data;
